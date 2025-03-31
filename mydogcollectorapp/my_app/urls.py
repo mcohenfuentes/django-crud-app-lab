128e26fccbbd63837d50a107bd390543cefa9268
Mica CF <mcohenfuentes@gmail.com>
2025-03-31 17:02:49 -0400

    included static files in templates and rendered data in templates

--- mydogcollectorapp/my_app/urls.py
+++ mydogcollectorapp/my_app/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
    path('', views.home, name='home'),
    path('about/', views.about, name='about'),
+   path('mountains/', views.mountain_index, name='mountain-index'),
 ]
--- mydogcollectorapp/my_app/views.py
+++ mydogcollectorapp/my_app/views.py
@@ -1,9 +1,27 @@
 from django.shortcuts import render
 
-from django.http import HttpResponse
+
+
+class Mountain:
+    def __init__(self, name, location, description):
+        self.name = name
+        self.location = location
+        self.description = description
+        
+
+
+mountains = [
+    Mountain('Saint Elias', 'North America', 'Second Tallest Mountain in North America'),
+    Mountain('Denali', 'North America', 'Highest Peak'),
+    Mountain('Mount Ranier', 'North America', 'Beautiful'),
+]
+
+def mountain_index(request):
+    # Render the cats/index.html template with the cats data
+    return render(request, 'mountains/index.html', {'mountains': mountains})
 
 def home(request):
-    return HttpResponse('<h1>Hello Mountains</h1>')
+    return render(request, 'home.html')
 
 def about(request):
     return render(request, 'about.html')
